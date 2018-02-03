872da8fb27778b2f2fd7cae53ce82c97a63c073c
Kalyan <kalyanpagadala@gmail.com>
2018-02-03 15:35:46 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,76 @@
-## Put comments here that give an overall description of what your
-## functions do
+##################################################################################
+#
+#	Author: Kalyan
+#	Platform: R(3.4.3) x86_64-w64-mingw32/x64 (64-bit)
+#	Date: 3 Feb 2018
+#
+#	Description: 
+#               Defines two function- 
+# 	        makeCacheMatrix: creates a special list of 4 function to handle a matrix and cache its inverse. Takes a matrix as input. Computes cache on initialisation
+# 	        cacheInverse: 	 computes the cache of the special "Matrix" and stores it. If cache already exists, it just retreives the inverse.
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix())
+{
+	inv <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+	set <- function(y)
+	{
+		x <<- y
+		inv <<- NULL
+	}
 
+	get <- function()
+	{
+		x
+	}
+
+	setInv <- function(inver)
+	{
+		inv <<- inver
+	}
+
+	getInv <- function()
+	{
+		inv
+	}
+
+	# Added a small feauture where the cache of the matirx is directly computed upon the initialisation
+
+	thisList <- list(set = set, get = get, setInv = setInv, getInv = getInv)
+
+	setInv(cacheInverse(thisList))
+
+	thisList
 }
 
+cacheInverse <- function(x)
+{
+	inv <- x$getInv()
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	if(is.null(inv))
+	{
+		inv <- solve(x$get())
+		x$setInv(inv)
+		return(inv)
+	}
+	print('Cached Inv: ')
+	inv
 }
+
+myMat = matrix(c(2, 3, 5, 1, 6, 1, 5, 3, 3), 3, 3)
+myBetterMat <- makeCacheMatrix(myMat)
+
+# Cached inverse already exists
+myBetterMat$getInv()
+# Cached inverse already exists
+cacheInverse(myBetterMat)
+
+newMat = matrix(c(1, 3, 6, 2, 3, 1, 2, 8, 3), 3, 3)
+myBetterMat$set(newMat)
+
+# Matrix updated therefore cache needs to be recalculated
+myBetterMat$getInv()
+
+cacheInverse(myBetterMat)
+
+
